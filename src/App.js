2d0111a45566b1d4801b7fc34d8f20a7d6a500a1
Rahul Thawal <iamrahulthawal.com>
2023-06-16 19:05:28 -0400

    Added Condition to load the data else show no record

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() => {
     setGladiatorData(GladiatorService.get());
-  }, [gladiatorData]);
+  });
 
   return (
     <div style={{
@@ -21,16 +21,21 @@ function App() {
         <div className="row">
           <div className="col-12 col-sm-8 col-lg-5">
             <ul className="list-group">
-              {gladiatorData.map((data,index) => (
-                <a  key={index} href="/#" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                  <div className="flex-column">
-                    <p><small> Glaidator Name: {data.gladiator_name}</small></p>
-                    <p><small>Real Name: {data.real_name}</small></p>
-                    <p><small> First Year: {data.first_year}</small></p>
-                    <p><small>  Last year: {data.last_year}</small></p>
-                  </div>
-                </a>
-              ))}
+              {gladiatorData && gladiatorData.length ? (
+                gladiatorData.map((data, index) => (
+                  <a key={index} href="/#" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                    <div className="flex-column">
+                      <p><small> Glaidator Name: {data.gladiator_name}</small></p>
+                      <p><small>Real Name: {data.real_name}</small></p>
+                      <p><small> First Year: {data.first_year}</small></p>
+                      <p><small>  Last year: {data.last_year}</small></p>
+                    </div>
+                  </a>
+                ))
+              ) : (
+                <h1 variant="primary">No Records found</h1>
+              )
+              }
             </ul>
           </div>
         </div>
